c6c0f70730a3599c26f8f3e3e062957275162938
yaojunxu <yaojunxu@ensource.cn>
2021-02-23 11:40:30 +0800

    00

--- hop/src/views/login/login.vue
+++ hop/src/views/login/login.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="login_page fillcontain">
+    <img class="bgImg" src="../../assets/bg4.jpeg">
     <transition name="form-fade" mode="in-out">
       <section class="form_contianer" v-show="showLogin">
         <div class="manage_tip">
@@ -26,45 +27,52 @@
 
 <script>
 export default {
-  name: "login.vue"
+  data(){
+    return {
+      loginForm: {
+        username: '',
+        password: '',
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ],
+      },
+      showLogin: true,
+    }
+  },
+  mounted(){
+
+  },
+  methods: {
+    submitForm (form) {
+
+    }
+  },
+  watch: {
+
+  }
 }
 </script>
 
 <style scoped>
-.login_page{
-  background-color: #324057;
-}
-.manage_tip{
-position: absolute;
-width: 100%;
-top: -100px;
-left: 0;
-  p{
-    font-size: 34px;
-    color: #fff;
-  }
+.bgImg {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow: hidden;
+  top: 0%;
+  left: 0%;
+  z-index: -1;
 }
 .form_contianer{
-.wh(320px, 210px);
-.ctp(320px, 210px);
-  padding: 25px;
-  border-radius: 5px;
-  text-align: center;
-  background-color: #fff;
-  .submit_btn{
-    width: 100%;
-    font-size: 16px;
-  }
-}
-.tip{
-  font-size: 12px;
-  color: red;
-}
-.form-fade-enter-active, .form-fade-leave-active {
-  transition: all 1s;
-}
-.form-fade-enter, .form-fade-leave-active {
-  transform: translate3d(0, -50px, 0);
-  opacity: 0;
+  background-color: whitesmoke;
+  width: 30%;
+  position: absolute;
+  top:30%;
+  left: 30%;
 }
 </style>
